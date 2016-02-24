5830da795f9d77269e31d809734e6c4ec4b513d0
rrjoshi92 <rrjoshi92@gmail.com>
2016-02-24 04:40:27 -0800

    chat app added.

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ // res.render('index', { title: 'Express' });
+ 
+ 
+// res.sendFile('index.html');
+res.sendfile(__dirname + '/public/index.html');
+});
+
+
+
+
+
+module.exports = router;
